575ed71efe82357eac38fa3280e67a9d7d7f9032
shogo enomoto <202411-AP-01-shogo-enomoto>
2025-01-22 12:26:03 +0900

    #33886　２ページ目以降、ページャーの表示がおかしくなる

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -120,7 +120,7 @@ function subStockView($param)
 			return;
 		}
 
-		$sql = fnSqlStockList(1, $param);
+		$sql = fnSqlStockList(0, $param);
 		$res = mysqli_query($param["conn"], $sql);
 		$row = mysqli_fetch_array($res);
 
